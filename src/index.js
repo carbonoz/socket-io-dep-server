23c8782102a52f27dd13c9f9edafa96d06419dd9
akashi7 <christiannseko@gmail.com>
2024-11-27 17:15:59 +0200

    remove-socket

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { createServer } from 'http'
 import { scheduleJob } from 'node-schedule'
-import WebSocket from 'ws'
 import { connectDatabase, disconnectDatabase, prisma } from './config/db'
 import { redisClient } from './config/redis.db'
 import { saveToMongoDb } from './utils/mongo'
@@ -10,43 +9,6 @@ import { getMeanValues } from './utils/redis'
 const app = express()
 const server = createServer(app)
 
-const server1Url = 'https://broker.carbonoz.com:8000'
-// const server1Url = 'ws://localhost:8000'
-
-const reconnectDelay = 5000 // Delay in milliseconds for reconnection
-
-const connectWebSocket = () => {
-  const ws = new WebSocket(server1Url)
-
-  ws.on('open', () => {
-    console.log('Connected to WebSocket Server 1')
-
-    ws.on('message', (message) => {
-      const messageString = message?.toString()
-      try {
-        // Handle message parsing
-      } catch (error) {
-        console.error('Error parsing message:', error)
-      }
-    })
-
-    ws.on('close', () => {
-      console.log('WebSocket connection closed, attempting to reconnect...')
-      setTimeout(connectWebSocket, reconnectDelay)
-    })
-
-    ws.on('error', (err) => {
-      console.error('WebSocket Error:', err.message)
-      console.error('Error Details:', err)
-      // Don't kill the app on WebSocket error, just log and reconnect
-    })
-  })
-
-  ws.on('error', (err) => {
-    console.error('WebSocket connection failed:', err.message)
-    setTimeout(connectWebSocket, reconnectDelay)
-  })
-}
 
 redisClient
   .connect()
@@ -60,7 +22,6 @@ redisClient.on('error', (err) => {
   console.log('Redis Client Connection Error', err)
 })
 
-connectWebSocket()
 
 app.get('/', async (req, res) => {
   try {
